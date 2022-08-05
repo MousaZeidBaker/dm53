7eb50d6101049b0c43101b17b47e6239767e6472
Mousa Zeid Baker <mzeb@moodagent.com>
2022-08-05 12:28:06 +0200

    fix: lint

--- src/dm53/main.py
+++ src/dm53/main.py
@@ -18,7 +18,7 @@ log_level = os.environ.get("LOG_LEVEL", "INFO").upper()
 logging.basicConfig(
     level=log_level,
     format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
-    datefmt='%Y-%m-%d %H:%M:%S',
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 logger = logging.getLogger("dm53")
 
